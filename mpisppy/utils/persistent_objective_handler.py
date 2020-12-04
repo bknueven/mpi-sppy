22d21367fec8bbecb1acaf9131148d3294b94c6e
Bernard Knueven <Bernard.Knueven@nrel.gov>
2020-12-04 12:37:51 -0700

    some notes

--- mpisppy/utils/persistent_objective_handler.py
+++ mpisppy/utils/persistent_objective_handler.py
@@ -1,5 +1,12 @@
 # Copyright 2020 by B. Knueven, D. Mildebrath, C. Muir, J-P Watson, and D.L. Woodruff
 # This software is distributed under the 3-clause BSD License.
+
+## TODO: Ideally, this module would also handle:
+##        1. Bundles
+##        2. APH
+##      Such that the PHBase object could maintain
+##      objective function management smoothly
+
 import abc
 
 ## ABC for Objective Handlers
