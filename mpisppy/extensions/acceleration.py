e17b050bc40968787d1b042ca96f4cf2a7d2eed5
Bernard Knueven <Bernard.Knueven@nrel.gov>
2024-04-01 09:38:50 -0600

    checking in decelleration

--- mpisppy/extensions/acceleration.py
+++ mpisppy/extensions/acceleration.py
@@ -1,9 +1,9 @@
 # Copyright 2020 by B. Knueven, D. Mildebrath, C. Muir, J-P Watson, and D.L. Woodruff
 # This software is distributed under the 3-clause BSD License.
 
-from mpisppy.extensions.norm_rho_updater import NormRhoUpdater
+from mpisppy.extensions.extension import Extension
 
-class PHAcceleration(NormRhoUpdater):
+class PHAcceleration(Extension):
     """ PH Acceleration via overrelaxation
         Overwrite all Extension methods to
         only get what we need from NormRhoUpdater
@@ -11,19 +11,8 @@ class PHAcceleration(NormRhoUpdater):
     def __init__(self, spopt_object):
         super().__init__(spopt_object)
         self.ph = spopt_object
-        self.alpha = 0.9 # acceleration term
-        self.eta = 0.9 # guard/restart condition
-        self._gamma_init = None # first convergence criterion
-        self._gamma = None # convergence criterion
-        self._prev_avg = {}
+        self.alpha = 0.5 # decceleration term
         self._prev_W = {}
-
-    def get_current_step(self):
-        current_step = {}
-        for s in self.ph.local_scenarios.values():
-            for ndn_i, xbar in s._mpisppy_model.xbars.items():
-                current_step[ndn_i] = xbar.value
-            return current_step
 
     def setup_hub(self):
         '''
@@ -110,7 +99,7 @@ class PHAcceleration(NormRhoUpdater):
             completed, and we have ensured that none of the models are
             infeasible. The rho_setter, if present, has not yet been applied.
         '''
-        pass
+        self._snapshot_W(self.ph)
 
     def post_iter0_after_sync(self):
         ''' Method called after the first PH iteration, after the
@@ -118,17 +107,6 @@ class PHAcceleration(NormRhoUpdater):
             has completed.
         '''
         pass
-
-    def _compute_gamma_init(self):
-        primal_residual = self._compute_primal_residual_norm(self.ph)
-        dual_residual = self._compute_dual_residual_norm(self.ph)
-        self._gamma_init = {ndn_i : primal_residual[ndn_i] + dual_residual[ndn_i] for ndn_i in primal_residual}
-        return
-
-    def _compute_gamma(self):
-        primal_residual = self._compute_primal_residual_norm(self.ph)
-        dual_residual = self._compute_dual_residual_norm(self.ph)
-        return {ndn_i : primal_residual[ndn_i] + dual_residual[ndn_i] for ndn_i in primal_residual}
 
     def _snapshot_W(self, ph):
         for s in ph.local_scenarios.values():
@@ -142,46 +120,11 @@ class PHAcceleration(NormRhoUpdater):
             If a converger is present, this method is called between the
             convergence_value() method and the is_converged() method.
         '''
-
-        if not self._prev_avg:
-            self._snapshot_avg(self.ph)
-            self._snapshot_W(self.ph)
-            return
-        if self._gamma_init is None:
-            self._compute_gamma_init()
-
-        # compute acceleration
-        current_step = self.get_current_step()
-
-        # for the gamma_k computation
         for s in self.ph.local_scenarios.values():
-            for ndn_i, xbar in s._mpisppy_model.xbars.items():
-                xbar._value = current_step[ndn_i] + self.alpha * (current_step[ndn_i] - self._prev_avg[ndn_i])
+            for ndn_i, w in s._mpisppy_model.W.items():
+                s._mpisppy_model.W[ndn_i]._value = (1 - self.alpha) * w._value + self.alpha * self._prev_W[s][ndn_i]
 
-        gamma_k = self._compute_gamma()
-
-        for ndn_i in gamma_k:
-            #print(f"{ndn_i}, gamma_k: {gamma_k[ndn_i]}, initial: {self._gamma_init[ndn_i]}")
-            if gamma_k[ndn_i] < self._gamma_init[ndn_i] * self.eta:
-                # keep the inertia
-                print(f"over relaxing {ndn_i}")
-                current_step[ndn_i] = current_step[ndn_i] + self.alpha * (current_step[ndn_i] - self._prev_avg[ndn_i])
-                for s in self.ph.local_scenarios.values():
-                    s._mpisppy_model.W[ndn_i]._value = s._mpisppy_model.W[ndn_i]._value + self.alpha * (s._mpisppy_model.W[ndn_i]._value - self._prev_W[s][ndn_i])
-            #else:
-                #print(f"not over relaxing {ndn_i}")
-                # else we will keep the current_step
-
-        # store (new) xbars
-        for s in self.ph.local_scenarios.values():
-            for ndn_i, xbar in s._mpisppy_model.xbars.items():
-                xbar._value = current_step[ndn_i]
-
-        #self.ph.Update_W(verbose=False)
-
-        self._prev_avg = current_step
         self._snapshot_W(self.ph)
-        self._gamma_init = gamma_k
 
     def enditer(self):
         ''' Method called after the solve_loop(), but before the next x-bar and
