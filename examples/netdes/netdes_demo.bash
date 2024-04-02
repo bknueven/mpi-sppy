bbfafdfdf26964d02c8e1d3217c215dabb460228
Bernard Knueven <Bernard.Knueven@nrel.gov>
2024-04-02 09:06:25 -0600

    a few different experimental options"

--- examples/netdes/netdes_demo.bash
+++ examples/netdes/netdes_demo.bash
@@ -3,7 +3,7 @@
 SOLVER=xpress_persistent
 #SOLVER=cplex
 
-mpiexec -host=localhost -n 6 python -u -m mpi4py netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=1 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=100 --rel-gap=-1 --abs-gap=-1 --xhatshuffle --intra-hub-conv-thresh=-1.0 --presolve --lr-cross-scenario-cuts
-#mpiexec -host=localhost -n 6 python -u -m mpi4py netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=1 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=30 --rel-gap=0.01 --xhatshuffle --intra-hub-conv-thresh=-1.0 --presolve --cross-scenario-cuts
-#mpiexec -host=localhost -n 6 python -u -m mpi4py netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=1 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=30 --rel-gap=0.01 --xhatshuffle --intra-hub-conv-thresh=-1.0 --presolve --lagrangian
+mpiexec -host=localhost -n 6 python -u -m mpi4py netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=1 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=200 --rel-gap=-1 --abs-gap=-1 --xhatshuffle --intra-hub-conv-thresh=-1.0 --presolve --lr-cross-scenario-cuts
+#mpiexec -host=localhost -n 6 python -u -m mpi4py netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=1 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=100 --rel-gap=-1 --abs-gap=-1 --xhatshuffle --intra-hub-conv-thresh=-1.0 --presolve --cross-scenario-cuts
+#mpiexec -host=localhost -n 6 python -u -m mpi4py netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=1 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=100 --rel-gap=-1  --abs-gap=-1 --xhatshuffle --intra-hub-conv-thresh=-1.0 --presolve --lagrangian
 #mpiexec -host=localhost -n 6 python -u -m mpi4py netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=1 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=30 --rel-gap=0.01 --xhatshuffle --intra-hub-conv-thresh=-1.0 --presolve --fwph
