db35291ce825c2204abc8e7a6909893dda1e95fe
Bernard Knueven <Bernard.Knueven@nrel.gov>
2025-01-07 14:01:40 -0700

    refactor ahead of major change

--- mpisppy/cylinders/ph_xhat.py
+++ mpisppy/cylinders/ph_xhat.py
@@ -29,14 +29,25 @@ logger = logging.getLogger("mpisppy.cylinders.ph_xhat")
 class PHXhat(XhatShuffleInnerBound):
     converger_spoke_char = "P"
 
-    def xhat_opt(self):
-        return PHBase
+    def _vb(self, msg):
+        if self.verbose and self.opt.cylinder_rank == 0:
+            print("(rank0) " + msg)
 
-    def main(self):
-        logger.debug(f"Entering main on ph_xhat spoke rank {self.global_rank}")
+    def _new_nonant_debug_msg(self, xh_iter):
+        # similar to above, not all ranks will agree on
+        # when there are new_nonants (in the same loop)
+        logger.debug(f"   *Xhatshuffle loop iter={xh_iter}")
+        logger.debug(f"   *got a new one! on rank {self.global_rank}")
+        logger.debug(f"   *localnonants={str(self.localnonants)}")
+        self._vb("  New nonants")
 
-        self.opt.PH_Prep()
-        self.xhat_prep()
+    def _loop_debug_msg(self, xh_iter):
+        logger.debug(
+            f"   Xhatshuffle loop iter={xh_iter} on rank {self.global_rank}"
+        )
+        logger.debug(f"   Xhatshuffle got from opt on rank {self.global_rank}")
+
+    def _set_options(self):
         if "reverse" in self.opt.options["ph_xhat_options"]:
             self.reverse = self.opt.options["ph_xhat_options"]["reverse"]
         else:
@@ -56,6 +67,100 @@ class PHXhat(XhatShuffleInnerBound):
         self.solver_options = self.opt.options["ph_xhat_options"]["xhat_solver_options"]
 
         self.verbose = True
+
+    def xhat_opt(self):
+        return PHBase
+
+    def restart_ph(self):
+        # set the nonants to those coming from Hub
+        self.opt._put_nonant_cache(self.localnonants)
+        self.opt._restore_nonants(update_persistent=False)
+        self.opt._restore_original_fixedness()
+
+        # reset Ws
+        for s in self.opt.local_scenarios.values():
+            for w in s._mpisppy_model.W.values():
+                w._value = 0
+        self.opt.Compute_Xbar()
+        self.opt.Update_W(verbose=False)
+        # TODO: add smoothing
+        smoothed = False
+        if smoothed:
+            self.opt.Update_z(self.verbose)
+        # fix a bunch
+        # first = True
+        raw_fixed = 0
+        for k, s in self.opt.local_scenarios.items():
+            for ndn_i, xvar in s._mpisppy_data.nonant_indices.items():
+                if xvar.is_fixed():
+                    continue
+                xb = pyo.value(s._mpisppy_model.xbars[ndn_i])
+                diff = xb * xb - pyo.value(s._mpisppy_model.xsqbars[ndn_i])
+                totval = self.fixtol * self.fixtol
+                # if first:
+                #     print(f"{xvar.name}, {xb=}, {diff=}, {totval=}")
+                if -diff < totval and diff < totval:
+                    # if first:
+                    #     print("\tfixing")
+                    if xvar.is_integer():
+                        if math.isclose(
+                            int(xb), xb, abs_tol=1e-5
+                        ):
+                            xvar.fix(int(xb))
+                            raw_fixed += 1
+                    elif xvar.lb is not None and xvar.lb > xb:
+                        xvar.fix(xvar.lb)
+                        raw_fixed += 1
+                    elif xvar.ub is not None and xvar.ub < xb:
+                        xvar.fix(xvar.ub)
+                        raw_fixed += 1
+                    else:
+                        xvar.fix(xb)
+                        raw_fixed += 1
+                # else:
+                #     if first:
+                #         print("\tnot fixing")
+            # first = False
+
+        number_fixed = int(raw_fixed / len(self.opt.local_scenarios))
+        self._vb(f"  Fixed {number_fixed} non-anticipative varibles")
+        self.opt._save_nonants()
+
+    def ph_iter(self):
+        if self.opt.extensions:
+            self.opt.extobject.miditer()
+
+        self._vb("  Doing PH iteration")
+        teeme = False
+        dtiming = False
+        self.opt.solve_loop(
+            solver_options=self.solver_options,
+            dtiming=dtiming,
+            gripe=True,
+            disable_pyomo_signal_handling=False,
+            tee=teeme,
+            verbose=False,  # self.verbose
+        )
+        self.opt._save_nonants()
+
+        if self.opt.extensions:
+            self.opt.extobject.enditer()
+            self.opt.extobject.enditer_after_sync()
+
+        self.opt.Compute_Xbar()
+        self.opt.Update_W(verbose=False)
+        # TODO: add smoothing
+        smoothed = False
+        if smoothed:
+            self.opt.Update_z(self.verbose)
+
+    def main(self):
+        logger.debug(f"Entering main on ph_xhat spoke rank {self.global_rank}")
+
+        self.opt.PH_Prep()
+        self.xhat_prep()
+        self._set_options()
+
         # give all ranks the same seed
         self.random_stream.seed(self.random_seed)
 
@@ -69,12 +174,8 @@ class PHXhat(XhatShuffleInnerBound):
             shuffled_scenarios, self.opt.nonleaves, self.reverse, self.iter_step
         )
 
-        def _vb(msg):
-            if self.verbose and self.opt.cylinder_rank == 0:
-                print("(rank0) " + msg)
-
         if self.opt.rho_setter is not None:
-            _vb("PHXhat calling rho setter")
+            self._vb("PHXhat calling rho setter")
             self.opt._use_rho_setter(False)
 
         # update rho
@@ -82,18 +183,6 @@ class PHXhat(XhatShuffleInnerBound):
         for scenario in self.opt.local_scenarios.values():
             for ndn_i in scenario._mpisppy_model.rho:
                 scenario._mpisppy_model.rho[ndn_i] *= rf
-
-        # _vb(f"  Doing PH iter 0")
-        # teeme = False
-        # dtiming = False
-        # self.opt.solve_loop(
-        #     solver_options=self.solver_options,
-        #     dtiming=dtiming,
-        #     gripe=True,
-        #     disable_pyomo_signal_handling=False,
-        #     tee=teeme,
-        #     verbose=False, #self.verbose
-        # )
 
         xh_iter = 1
         iter_no_improve = 1_000_000_000
@@ -105,10 +194,7 @@ class PHXhat(XhatShuffleInnerBound):
                 continue
 
             if (xh_iter - 1) % 100 == 0:
-                logger.debug(
-                    f"   Xhatshuffle loop iter={xh_iter} on rank {self.global_rank}"
-                )
-                logger.debug(f"   Xhatshuffle got from opt on rank {self.global_rank}")
+                self._loop_debug_msg(xh_iter)
 
             # check if we don't already have new nonants
             if not new_nonants:
@@ -116,127 +202,31 @@ class PHXhat(XhatShuffleInnerBound):
             if new_nonants and iter_no_improve > 1:
                 new_nonants = False
                 best_obj_this_nonants = float("inf")
-                # similar to above, not all ranks will agree on
-                # when there are new_nonants (in the same loop)
-                logger.debug(f"   *Xhatshuffle loop iter={xh_iter}")
-                logger.debug(f"   *got a new one! on rank {self.global_rank}")
-                logger.debug(f"   *localnonants={str(self.localnonants)}")
-                _vb("  New nonants")
 
-                # update the caches
-                self.opt._put_nonant_cache(self.localnonants)
-                self.opt._restore_nonants(update_persistent=False)
-                self.opt._restore_original_fixedness()
+                self._new_nonant_debug_msg(xh_iter)
 
-                # reset Ws
-                for s in self.opt.local_scenarios.values():
-                    for w in s._mpisppy_model.W.values():
-                        w._value = 0
+                self.restart_ph()
 
-                # scenario_cycler.begin_epoch()
-                # next_scendict = scenario_cycler.get_next()
-                # _vb(f"   Trying next {next_scendict}")
-                # update = self.try_scenario_dict(next_scendict)
-                # if update:
-                #     _vb(f"   Updating best to {next_scendict}")
-                #     scenario_cycler.best = next_scendict["ROOT"]
-
-                self.opt._restore_nonants(update_persistent=False)
-
-                self.opt.Compute_Xbar()
-                self.opt.Update_W(verbose=False)
-                # TODO: add smoothing
-                smoothed = False
-                if smoothed:
-                    self.opt.Update_z(self.verbose)
-                # fix a bunch
-                # first = True
-                raw_fixed = 0
-                for k, s in self.opt.local_scenarios.items():
-                    for ndn_i, xvar in s._mpisppy_data.nonant_indices.items():
-                        if xvar.is_fixed():
-                            continue
-                        xb = pyo.value(s._mpisppy_model.xbars[ndn_i])
-                        diff = xb * xb - pyo.value(s._mpisppy_model.xsqbars[ndn_i])
-                        totval = self.fixtol * self.fixtol
-                        # if first:
-                        #     print(f"{xvar.name}, {xb=}, {diff=}, {totval=}")
-                        if -diff < totval and diff < totval:
-                            # if first:
-                            #     print("\tfixing")
-                            if xvar.is_integer():
-                                if math.isclose(
-                                    int(xb), xb, abs_tol=1e-5
-                                ):
-                                    xvar.fix(int(xb))
-                                    raw_fixed += 1
-                            elif xvar.lb is not None and xvar.lb > xb:
-                                xvar.fix(xvar.lb)
-                                raw_fixed += 1
-                            elif xvar.ub is not None and xvar.ub < xb:
-                                xvar.fix(xvar.ub)
-                                raw_fixed += 1
-                            else:
-                                xvar.fix(xb)
-                                raw_fixed += 1
-                        # else:
-                        #     if first:
-                        #         print("\tnot fixing")
-                    # first = False
-
-                number_fixed = int(raw_fixed / len(self.opt.local_scenarios))
-                _vb(f"  Fixed {number_fixed} non-anticipative varibles")
-                self.opt._save_nonants()
-
-            _vb(f"    scenario_cycler._scenarios_this_epoch {scenario_cycler._scenarios_this_epoch}")
+            self._vb(f"    scenario_cycler._scenarios_this_epoch {scenario_cycler._scenarios_this_epoch}")
             # Restore nonants; compute xbar, W, fix lots of things, solve
             self.opt._restore_nonants(update_persistent=True)
             self.opt.reenable_W_and_prox()
 
-            if self.opt.extensions:
-                self.opt.extobject.miditer()
-
-            _vb("  Doing PH iteration")
-            _vb(f"  Solver: {s._solver_plugin}")
-            teeme = False
-            dtiming = False
-            self.opt.solve_loop(
-                solver_options=self.solver_options,
-                dtiming=dtiming,
-                gripe=True,
-                disable_pyomo_signal_handling=False,
-                tee=teeme,
-                verbose=False,  # self.verbose
-            )
-            self.opt._save_nonants()
-
-            if self.opt.extensions:
-                self.opt.extobject.enditer()
-                self.opt.extobject.enditer_after_sync()
-
-            self.opt.Compute_Xbar()
-            self.opt.Update_W(verbose=False)
-            # TODO: add smoothing
-            smoothed = False
-            if smoothed:
-                self.opt.Update_z(self.verbose)
+            self.ph_iter()
 
             self.opt.disable_W_and_prox()
 
-            # # until proven otherwise
-            # better_this_iter = False
             old_best_obj = best_obj_this_nonants
             scenario_cycler.begin_epoch()
             next_scendict = scenario_cycler.get_next()
-            _vb(f"   Trying next {next_scendict}")
+            self._vb(f"   Trying next {next_scendict}")
             update, obj = self.try_scenario_dict(next_scendict)
             obj = obj if self.is_minimizing else -obj
             if obj < best_obj_this_nonants:
                 best_obj_this_nonants = obj
-                _vb(f"   Updating best to {next_scendict}")
+                self._vb(f"   Updating best to {next_scendict}")
                 scenario_cycler.best = next_scendict["ROOT"]
                 iter_no_improve = 0
-                # better_this_iter = True
 
             xh_iter += 1
 
@@ -245,19 +235,18 @@ class PHXhat(XhatShuffleInnerBound):
                 continue
 
             self.opt._restore_nonants(update_persistent=True)
-            _vb(f"   Trying next {next_scendict}")
+            self._vb(f"   Trying next {next_scendict}")
             update, obj = self.try_scenario_dict(next_scendict)
             obj = obj if self.is_minimizing else -obj
             if obj < best_obj_this_nonants:
                 best_obj_this_nonants = obj
-                _vb(f"   Updating best to {next_scendict}")
+                self._vb(f"   Updating best to {next_scendict}")
                 scenario_cycler.best = next_scendict["ROOT"]
                 iter_no_improve = 0
-                # better_this_iter = True
 
             if old_best_obj == best_obj_this_nonants:
                 iter_no_improve += 1
 
-            _vb(f"   iter_no_improve: {iter_no_improve}")
+            self._vb(f"   iter_no_improve: {iter_no_improve}")
 
             xh_iter += 1
